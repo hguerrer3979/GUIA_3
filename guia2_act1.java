705e116e127ec4584a020e99eca59c91db3396ca
hguerrer3979 <32824313+hguerrer3979@users.noreply.github.com>
2018-03-19 22:05:35 -0500

    Update guia2_act1.java

--- guia2_act1.java
+++ guia2_act1.java
@@ -1,8 +1,3 @@
-//========================================================================
-// This conversion was produced by the Free Edition of
-// C++ to Java Converter courtesy of Tangible Software Solutions.
-// Order the Premium Edition at https://www.tangiblesoftwaresolutions.com
-//========================================================================
 
 public class guia2_act1
 {
